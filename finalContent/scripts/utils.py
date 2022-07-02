ce4317c50812e8f80712ace6b73e3aea36ece4b6
suryajayaraman <suryajayaraman97@gmail.com>
2022-07-02 15:10:25 +0530

    combined fucntions to single script

--- finalContent/scripts/utils.py
+++ finalContent/scripts/utils.py
@@ -3,11 +3,99 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+from collections import namedtuple
 
 # DL library imports
 import torch
-from torch.utils.data import Dataset
-from bdd100k_utils import inverse_transform, train_id_to_color
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader
+
+
+###################################
+# FILE CONSTANTS
+###################################
+
+# Convert to torch tensor and normalize images using Imagenet values
+preprocess = transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=(0.485, 0.56, 0.406), std=(0.229, 0.224, 0.225))
+                ])
+
+# when using torch datasets we defined earlier, the output image
+# is normalized. So we're defining an inverse transformation to 
+# transform to normal RGB format
+inverse_transform = transforms.Compose([
+        transforms.Normalize((-0.485/0.229, -0.456/0.224, -0.406/0.225), (1/0.229, 1/0.224, 1/0.225))
+    ])
+
+
+# Constants for Standard color mapping
+# reference : https://github.com/bdd100k/bdd100k/blob/master/bdd100k/label/label.py
+
+Label = namedtuple( "Label", [ "name", "train_id", "color"])
+drivables = [ 
+             Label("direct", 0, (219, 94, 86)),        # red
+             Label("alternative", 1, (86, 211, 219)),  # cyan
+             Label("background", 2, (0, 0, 0)),        # black          
+            ]
+train_id_to_color = [c.color for c in drivables if (c.train_id != -1 and c.train_id != 255)]
+train_id_to_color = np.array(train_id_to_color)
+
+
+#####################################
+### TORCH DATASET CLASS DEFINITION ##
+#####################################
+
+
+class BDD100k_dataset(Dataset):
+    def __init__(self, images, labels, tf=None):
+        """Dataset class for BDD100k_dataset drivable / segmentation data """
+        self.images = images
+        self.labels = labels
+        self.tf = tf
+    
+    def __len__(self):
+        return self.images.shape[0]
+  
+    def __getitem__(self, index):
+        # read source image and convert to RGB, apply transform
+        rgb_image = self.images[index]
+        if self.tf is not None:
+            rgb_image = self.tf(rgb_image)
+
+        # read label image and convert to torch tensor
+        label_image  = torch.from_numpy(self.labels[index]).long()
+        return rgb_image, label_image  
+
+
+
+###################################
+# FUNCTION TO GET TORCH DATASET  #
+###################################
+
+def get_datasets(images, labels):
+    data = BDD100k_dataset(images, labels, tf=preprocess)
+
+    # split train data into train, validation and test sets
+    total_count = len(data)
+    train_count = int(0.7 * total_count) 
+    valid_count = int(0.2 * total_count)
+    test_count = total_count - train_count - valid_count
+    train_set, val_set, test_set = torch.utils.data.random_split(data, 
+                (train_count, valid_count, test_count), generator=torch.Generator().manual_seed(1))
+    return train_set, val_set, test_set
+
+
+###################################
+# FUNCTION TO GET TORCH DATALOADER  #
+###################################
+
+def get_dataloaders(train_set, val_set, test_set):
+    train_dataloader = DataLoader(train_set, batch_size=8,drop_last=True)
+    val_dataloader   = DataLoader(val_set, batch_size=8)
+    test_dataloader  = DataLoader(test_set, batch_size=8)
+    return train_dataloader, val_dataloader, test_dataloader    
+
 
 
 ###################################
