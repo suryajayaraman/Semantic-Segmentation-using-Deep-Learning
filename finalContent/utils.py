5c46e088cc34c177d30aa2cc22321564002ed59c
suryajayaraman <suryajayaraman97@gmail.com>
2022-07-13 07:52:32 +0530

    poly LR scheduler implementation added

--- finalContent/utils.py
+++ finalContent/utils.py
@@ -12,6 +12,7 @@ import torch
 import torch.nn as nn
 from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader
+from torch.optim.lr_scheduler import _LRScheduler
 
 # For dice loss function
 import segmentation_models_pytorch as smp
@@ -167,6 +168,51 @@ class pspnet_loss(nn.Module):
             loss = self.loss_fn(preds, labels)
         return loss
 
+
+
+###################################
+# POLY LR DECAY SCHEDULER DEFINITION
+###################################
+
+class polynomial_lr_decay(_LRScheduler):
+    """Polynomial learning rate decay until step reach to max_decay_step
+    
+    Args:
+        optimizer (Optimizer): Wrapped optimizer.
+        max_decay_steps: after this step, we stop decreasing learning rate
+        end_learning_rate: scheduler stoping learning rate decay, value of learning rate must be this value
+        power: The power of the polynomial.
+    
+    Reference:
+        https://github.com/cmpark0126/pytorch-polynomial-lr-decay
+    """
+    def __init__(self, optimizer, max_decay_steps, end_learning_rate=0.0001, power=1.0):
+        if max_decay_steps <= 1.:
+            raise ValueError('max_decay_steps should be greater than 1.')
+        self.max_decay_steps = max_decay_steps
+        self.end_learning_rate = end_learning_rate
+        self.power = power
+        self.last_step = 0
+        super().__init__(optimizer)
+        
+    def get_lr(self):
+        if self.last_step > self.max_decay_steps:
+            return [self.end_learning_rate for _ in self.base_lrs]
+
+        return [(base_lr - self.end_learning_rate) * 
+                ((1 - self.last_step / self.max_decay_steps) ** (self.power)) + 
+                self.end_learning_rate for base_lr in self.base_lrs]
+    
+    def step(self, step=None):
+        if step is None:
+            step = self.last_step + 1
+        self.last_step = step if step != 0 else 1
+        if self.last_step <= self.max_decay_steps:
+            decay_lrs = [(base_lr - self.end_learning_rate) * 
+                         ((1 - self.last_step / self.max_decay_steps) ** (self.power)) + 
+                         self.end_learning_rate for base_lr in self.base_lrs]
+            for param_group, lr in zip(self.optimizer.param_groups, decay_lrs):
+                param_group['lr'] = lr
 
 
 ###################################
